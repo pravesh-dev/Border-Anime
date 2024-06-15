b76014de407b3293534b1d22ccef4e14ec88bc03
pravesh dav <humantorch1932@gmail.com>
2024-06-15 19:09:11 +0530

    corrected color grdding

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let mover = document.querySelector("#mover");
 let boxModel =  box.getBoundingClientRect();
 box.addEventListener('mousemove', (e)=>{
 gsap.to(mover, {
+    scale: 1,
+    left: e.clientX - boxModel.left,
+    top: e.clientY - boxModel.top,
+})
+})
+box.addEventListener('mouseleave', (e)=>{
+gsap.to(mover, {
+    scale: 0,
     left: e.clientX - boxModel.left,
     top: e.clientY - boxModel.top,
 })
